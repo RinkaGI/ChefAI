36cae101b5662598ab55f7cd1d01157f9d031925
Rinka Is Developing lol <77236422+RinkaGI@users.noreply.github.com>
2023-02-25 14:51:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 import {createRequire} from 'module';
-import { ChatGPTUnofficialProxyAPI } from 'chatgpt';
+import receta from './backend/getRecipe.js';
 const require = createRequire(import.meta.url)
-
 const promptTerminal = require('prompt-sync')();
-const { ACCESS_TOKEN } = require('./config.json')
-
 
 // get info
 let ingredientes = promptTerminal("¿Qué ingredientes tienes?: ")
 let herramientas = promptTerminal("¿Qué herramientas quieres utilizar para cocinar? (air frier, microondas, batidora, barbacoa, horno etc..): ")
 let dificultad = promptTerminal("¿Qué dificultad quieres que sea hacerlo? (facil, normal, dificil): ")
 
-// get chatgpt proxy
-const api = new ChatGPTUnofficialProxyAPI({
-    accessToken: ACCESS_TOKEN
- })
+// get recipe and show it
+let recetaCocina = await receta(ingredientes, herramientas, dificultad);
 
- //check info
- if (dificultad != "facil" && dificultad != "normal" && dificultad != "dificil") {
-    console.log("La dificultad debe ser facil, normal o dificil")
- } 
-
- // get recipe and show it
- let res = await api.sendMessage(`Hola, tu nombre actual es ChefAI. Quiero que me digas una receta de cocina, va a tener estos ingredientes: ${ingredientes}. Para cocinar tengo esto: ${herramientas}. La dificultad de hacerlo va a ser ${dificultad}. Quiero que me digas los ingredientes y herramientas exactos, y también el procedimiento muy detallado. Vas a incluir el tiempo que dura cada cosa. Tiene que ser una receta existente. \n Quiero que tus respuestas sean así: \n **Ingredientes** \n - Lista de ingredientes \n **Dificultad** \n La dificultad de la receta \n **Herramientas** \n Lista de herramientas \n **Procedimiento** \n Texto muy detallado del procedimiento`);
-
-
- console.log(res.text)
+console.log(recetaCocina)
