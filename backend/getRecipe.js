1723e8f134357928696d9e9ac5e595bd71e609e4
Rinka Is Developing lol <77236422+RinkaGI@users.noreply.github.com>
2023-02-25 14:52:30 +0100

    Update getRecipe.js

--- backend/getRecipe.js
+++ backend/getRecipe.js
@@ -1,22 +1,13 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url )
 const {ACCESS_TOKEN} = require('../config.json');
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt';
 
-module.exports = async function obtenerReceta(ingredientes, herramientas, dificultad) {
-    import('chatgpt').then(({ ChatGPTUnofficialProxyAPI }) => {
-        const api = new ChatGPTUnofficialProxyAPI({
-            accessToken: ACCESS_TOKEN
-        })
-        
-        async function lmao() {
-            if (dificultad != "facil" && dificultad != "normal" && dificultad != "dificil") {
-                return "La dificultad debe ser facil, normal o dificil"
-            }
-            
-            let res = await api.sendMessage(```Hola, tu nombre actual es ChefAI. Quiero que me digas una receta de cocina, va a tener estos ingredientes: ${ingredientes}. Para cocinar tengo esto: ${herramientas}. La dificultad de hacerlo va a ser ${dificultad}. Quiero que me digas los ingredientes y herramientas exactos, y también el procedimiento muy detallado. Vas a incluir el tiempo que dura cada cosa. Tiene que ser una receta existente. \n Quiero que tus respuestas sean así: \n **Ingredientes** \n - Lista de ingredientes \n **Dificultad** \n La dificultad de la receta \n **Herramientas** \n  - Lista de herramientas \n **Procedimiento** \n Texto muy detallado del procedimiento```);
-            return res.text;
-        }
+const api = new ChatGPTUnofficialProxyAPI({
+    accessToken: ACCESS_TOKEN
+})
 
-        return lmao();
-      }).catch((error) => {
-        console.error(error)
-    });
+export default async function receta(ingredientes, herramientas, dificultad) {
+    let res = await api.sendMessage(`Hola, tu nombre actual es ChefAI. Quiero que me digas una receta de cocina, va a tener estos ingredientes: ${ingredientes}. Para cocinar tengo esto: ${herramientas}. La dificultad de hacerlo va a ser ${dificultad}. Quiero que me digas los ingredientes y herramientas exactos, y también el procedimiento muy detallado. Vas a incluir el tiempo que dura cada cosa. Tiene que ser una receta existente. \n Quiero que tus respuestas sean así: \n **Ingredientes** \n - Lista de ingredientes \n **Dificultad** \n La dificultad de la receta \n **Herramientas** \n Lista de herramientas \n **Procedimiento** \n Texto muy detallado del procedimiento`);
+    return res.text;
 }
